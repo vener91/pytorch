be76ef34c876a0a1b247e02a5c259af48b461fa4
Michael Voznesensky <voznesenskym@gmail.com>
2023-08-14 22:02:53 +0000

    woof

--- torch/_dynamo/symbolic_convert.py
+++ torch/_dynamo/symbolic_convert.py
@@ -1910,6 +1910,7 @@ class InstructionTranslatorBase(Checkpointable[InstructionTranslatorGraphState])
         )
         if name not in self.output.global_scope:
             self.output.install_global(name, value)
+
         return src
 
     @property
