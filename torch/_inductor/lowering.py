68823f6a1922865ef7e05aa847a91a7d39a92830
Oguz Ulgen <oulgen@meta.com>
2023-10-17 15:18:06 -0700

    [Inductor] Support user defined triton kernels in inductor
    ghstack-source-id: 4cf43a425c0649011dd106481813b3380452d48d
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/111434

--- torch/_inductor/lowering.py
+++ torch/_inductor/lowering.py
@@ -5,13 +5,14 @@ import os
 import warnings
 from collections import defaultdict
 from collections.abc import Iterable
-from typing import Any, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 import sympy
 
 import torch
 import torch.fx
 import torch.utils._pytree as pytree
+from torch._higher_order_ops.triton_kernel_wrap import triton_kernel_wrapper_mutation
 from torch._prims_common import (
     canonicalize_dim,
     canonicalize_dims,
@@ -252,7 +253,7 @@ def _register_lowering(
 
     @functools.wraps(decomp_fn)
     def wrapped(*args, **kwargs):
-        args: Union[List[Any], Tuple[Any, ...]] = list(args)
+        args: Union[List[Any], Tuple[Any, ...], Dict[Any, Any]] = list(args)
         unpacked = False
         # TODO maybe we need to use pytrees here
         if len(args) == 1 and isinstance(args[0], (list, tuple)):
@@ -4838,6 +4839,12 @@ def foobar(self, *args, **kwargs):
 def _realize(x):
     x.realize()
     return clone(x)
+
+
+@register_lowering(triton_kernel_wrapper_mutation)
+def triton_kernel_wrap(*, kernel_idx, grid, kwargs):
+    ir.UserDefinedTritonKernel(kernel_idx=kernel_idx, grid=grid, kernel_args=kwargs)
+    return {key: val for key, val in kwargs.items() if isinstance(val, TensorBox)}
 
 
 @register_lowering(torch.ops.inductor.accumulate_grad_)
